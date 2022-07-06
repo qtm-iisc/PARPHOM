Fixed velocity units

Phonon_Calculations/Examples/tbmos2_38.2/plot_bs_with_vel.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 #qfile = 'qpt_bands.dat'
-qfile = 'q_points_band.dat'
+qfile = 'qpt_bands_KKp.dat'
 
 with open(qfile,'r') as f:
     for nodes in f:
@@ -14,11 +14,16 @@ with open(qfile,'r') as f:
 f.close()
 nodes = nodes.split()
 nodes = np.array([int(i) for i in nodes])
-label = [r'$\mathbf{\Gamma}$', r'$\mathbf{M}$', r'$\mathbf{K}$', r'$\mathbf{\Gamma}$']
+label = [r'$\mathbf{\Gamma}$', 
+         r'$\mathbf{M}$', 
+         r'$\mathbf{K}$', 
+         r'$\mathbf{\Gamma}$',
+         r'$\mathbf{M}$',
+         r"$\mathbf{K^{'}}$"]
 
 
 #f = h5py.File('phbands_21.8.hdf5','r')
-f = h5py.File('phbands_21.8_fine.hdf5','r')
+f = h5py.File('phbands_38.2_mos2_KKp.hdf5','r')
 g = list(f.keys())
 
 eigval, vel = [], []
@@ -37,12 +42,12 @@ print(np.shape(vel))
 
 x = [i for i in range(eigval.shape[0])]
 #v = [[np.linalg.norm(vel[i,j]) for j in range(vel.shape[1])] for i in range(vel.shape[0])]
-v = np.linalg.norm(vel,axis=2)
+v = np.linalg.norm(vel,axis=2)/1000
 v_max = np.max(v)
 for i in range(eigval.shape[1]):
-    plt.plot(x,eigval[:,i],c='b',alpha=0.4,zorder=1)
+    plt.plot(x,eigval[:,i],c='gray',linewidth=0.8,zorder=1)
     plt.scatter(x,eigval[:,i],c=v[:,i],cmap='magma_r',vmin=0,vmax=v_max,
-                s=v[:,i]*5/1000,zorder=2)
+                s=v[:,i]*5,zorder=2)
 
 np.set_printoptions(precision=3,suppress=True)
 print(v[2])
@@ -50,13 +55,12 @@ print(v[2])
 for n in range(len(nodes)):
     plt.axvline(x=nodes[n],linewidth=1,color='k')
 plt.axhline(y=0,linewidth=1,color='k')
-plt.ylim(-1,600)
 plt.xlim(nodes[0],nodes[len(nodes)-1])
 plt.ylabel(r"Energy (cm$^{-1}$)",size=20)
 plt.xticks(nodes,label,fontsize=22)
 plt.tick_params(axis='both',labelsize=20)
 cbar = plt.colorbar()
-cbar.set_label('Velocity (m/s)',fontsize=18,rotation=270,labelpad=25)
+cbar.set_label('Velocity (km/s)',fontsize=18,rotation=270,labelpad=25)
 for t in cbar.ax.get_yticklabels():
      t.set_fontsize(20)
 plt.tight_layout()